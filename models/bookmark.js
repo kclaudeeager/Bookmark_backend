0785f49112a8f6543d04e400dd5e4900c7af8f77
Kwizera Claude <71407602+kclaudeeager@users.noreply.github.com>
2022-12-06 20:30:19 +0200

    first commit

---
+++ models/bookmark.js
@@ -0,0 +1,47 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+//book schema definition
+let BookMarkSchema = new Schema(
+  {
+    category:{
+        type:String,
+        required:true
+    },
+    title: { 
+        type: String, 
+        required: true 
+    },
+    author: {
+         type: String,
+         required: true 
+        },
+    description: { 
+        type: String,
+         required: true 
+        },
+    link:{
+        type: String,
+        required: true 
+    },
+    createdAt: { 
+        type: Date,
+        default: Date.now 
+        },    
+  }, 
+  { 
+    versionKey: false
+  }
+);
+
+// Sets the createdAt parameter equal to the current time
+BookMarkSchema.pre('save', next => {
+  now = new Date();
+  if(!this.createdAt) {
+    this.createdAt = now;
+  }
+  next();
+});
+
+//Exports the BookSchema for use elsewhere.
+module.exports = mongoose.model('bookmark', BookMarkSchema);
