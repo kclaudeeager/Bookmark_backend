5bbe4b15b4a55f7e5ef233729ce829619d3ea356
Kwizera Claude <71407602+kclaudeeager@users.noreply.github.com>
2022-12-07 05:39:42 +0200

    Connected to db

--- models/bookmark.js
+++ models/bookmark.js
@@ -1,47 +1,47 @@
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 //book schema definition
 let BookMarkSchema = new Schema(
   {
-    category:{
-        type:String,
-        required:true
+    category: {
+      type: String,
+      required: true,
     },
-    title: { 
-        type: String, 
-        required: true 
+    title: {
+      type: String,
+      required: true,
     },
     author: {
-         type: String,
-         required: true 
-        },
-    description: { 
-        type: String,
-         required: true 
-        },
-    link:{
-        type: String,
-        required: true 
+      type: String,
+      required: true,
     },
-    createdAt: { 
-        type: Date,
-        default: Date.now 
-        },    
-  }, 
-  { 
-    versionKey: false
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    versionKey: false,
   }
 );
 
 // Sets the createdAt parameter equal to the current time
-BookMarkSchema.pre('save', next => {
+BookMarkSchema.pre("save", (next) => {
   now = new Date();
-  if(!this.createdAt) {
+  if (!this.createdAt) {
     this.createdAt = now;
   }
   next();
 });
 
 //Exports the BookSchema for use elsewhere.
-module.exports = mongoose.model('bookmark', BookMarkSchema);
+module.exports = mongoose.model("bookmark", BookMarkSchema);
--- routes/bookmark.js
+++ routes/bookmark.js
@@ -1,69 +1,75 @@
-let mongoose = require('mongoose');
-let Bookmark = require('../models/bookmark');
+let mongoose = require("mongoose");
+let Bookmark = require("../models/bookmark");
 
 /*
  * GET /book route to retrieve all the books.
  */
 function getBookmarks(req, res) {
-    //Query the DB and if no errors, send all the books
-    let query = Bookmark.find({});
-    query.exec((err, bookmark) => {
-        if(err) res.send(err);
-        //If no errors, send them back to the client
-        res.json(bookmark);
-    });
+  //Query the DB and if no errors, send all the books
+  let query = Bookmark.find({});
+  query.exec((err, bookmark) => {
+    if (err) res.send(err);
+    //If no errors, send them back to the client
+    res.json(bookmark);
+  });
 }
 
 /*
  * POST /book to save a new book.
  */
 function postBookmark(req, res) {
-    //Creates a new book
-    var newBookmark= new Bookmark(req.body);
-    //Save it into the DB.
-    newBookmark.save((err,bookmark) => {
-        if(err) {
-            res.send(err);
-        }
-        else { //If no errors, send it back to the client
-            res.json({message: "Bookmark successfully added!", bookmark});
-        }
-    });
+  //Creates a new book
+  var newBookmark = new Bookmark(req.body);
+  //Save it into the DB.
+  newBookmark.save((err, bookmark) => {
+    if (err) {
+      res.send(err);
+    } else {
+      //If no errors, send it back to the client
+      res.json({ message: "Bookmark successfully added!", bookmark });
+    }
+  });
 }
 
 /*
  * GET /book/:id route to retrieve a book given its id.
  */
 function getBookmark(req, res) {
-    Bookmark.findById(req.params.id, (err, bookmark) => {
-        if(err) res.send(err);
-        //If no errors, send it back to the client
-        res.json(bookmark);
-    });        
+  Bookmark.findById(req.params.id, (err, bookmark) => {
+    if (err) res.send(err);
+    //If no errors, send it back to the client
+    res.json(bookmark);
+  });
 }
 
 /*
  * DELETE /book/:id to delete a book given its id.
  */
 function deleteBookmark(req, res) {
-    Bookmark.remove({_id : req.params.id}, (err, result) => {
-        if(err) res.send(err);
-        res.json({ message: "Bookmark successfully deleted!", result });
-    });
+  Bookmark.remove({ _id: req.params.id }, (err, result) => {
+    if (err) res.send(err);
+    res.json({ message: "Bookmark successfully deleted!", result });
+  });
 }
 
 /*
  * PUT /book/:id to updatea a book given its id
  */
 function updateBookmark(req, res) {
-    Bookmark.findById({_id: req.params.id}, (err, bookmark) => {
-        if(err) res.send(err);
-        Object.assign(bookmark, req.body).save((err, bookmark) => {
-            if(err) res.send(err);
-            res.json({ message: 'Booknark updated!', bookmark });
-        });    
+  Bookmark.findById({ _id: req.params.id }, (err, bookmark) => {
+    if (err) res.send(err);
+    Object.assign(bookmark, req.body).save((err, bookmark) => {
+      if (err) res.send(err);
+      res.json({ message: "Booknark updated!", bookmark });
     });
+  });
 }
 
 //export all the functions
-module.exports = { getBookmarks, postBookmark, getBookmark, deleteBookmark, updateBookmark };
+module.exports = {
+  getBookmarks,
+  postBookmark,
+  getBookmark,
+  deleteBookmark,
+  updateBookmark,
+};
